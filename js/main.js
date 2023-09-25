9fc9278138803ce06f7783882873bedcbbce73a1
f-a-d <mf30012022@gmail.com>
2023-09-26 01:44:52 +0700

    coba responsive 2

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const nav = document.getElementById('navContainer');
 
 window.addEventListener('scroll', () => {
   if (window.scrollY > 1) {
-    console.log('masuk');
+    nav.style.overflow = "hidden"
     nav.classList.add('scroll');
     nav.classList.add('bg-white/50'); // Menghapus kelas 'bg-white/20'
   } else {
